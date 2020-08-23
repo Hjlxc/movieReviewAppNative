02239246f30cab405a64709eef8ea55240568c84
Hjlxc <xcliu0324@gmail.com>
2020-08-22 22:01:26 -0400

    searchBox test file[WIP]

---
+++ __tests__/container/SearchBox-test.js
@@ -0,0 +1,10 @@
+import 'react-native';
+import React from 'react';
+import {render, cleanup, fireEvent} from '@testing-library/react-native';
+import renderer from 'react-test-renderer';
+
+// import SearchBox from '../../container/SearchBox';
+
+test('test', () => {
+  //   renderer.create(<SearchBox />);
+});
